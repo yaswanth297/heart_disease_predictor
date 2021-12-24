bc4811bae5c9ffbb0dbb47dfa2e6d26f9d960e5e
yaswanth297 <96608812+yaswanth297@users.noreply.github.com>
2021-12-24 11:18:59 +0530

    Add files via upload

---
+++ heart_disease_predictor.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+
+file=pd.read_csv('C:/Users/gnbae/Downloads/heart_failure_dataset.csv')
+
+X=file.drop(columns=['DEATH_EVENT'])
+
+y=file['DEATH_EVENT']
+
+model=DecisionTreeClassifier()
+model.fit(X,y)
+
+
+
+prediction=model.predict([[74,1,580,1,20,1,265000,2,130,1,1,5],[50,0,196,0,45,0,395000,1.6,136,1,1,285]])
+prediction
+
+
+# In[18]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
